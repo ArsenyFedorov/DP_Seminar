c1eb57b5abb03035ba2c21d383a545f86416ac5d
Арсений <ars071026@example.com>
2023-05-28 20:23:47 +0500

    Task_1 Seminar 1

--- Seminar_1/Task_1.py
+++ Seminar_1/Task_1.py
@@ -1,5 +1,7 @@
 # На вход подается число с плавающей точкой, выведите первую цифра дробной части
-float_num = float(input("Ввидите число с плавающей точкой:"))
+float_num = input("Ввидите число с плавающей точкой:")
+float_num = float_num.replace("-", "")
+float_num = float(float_num)
 float_num *= 10
 float_num = int(float_num)
-print(f"Первая цифра после запятой:{float_num % 10}")
+print(f"Первая цифра после запятой:{float_num% 10}")
